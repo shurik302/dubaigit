532b0bef4d3b2a5fbaf9748b09b0b3218f679f97
shurik302 <balabuha1988@gmail.com>
2022-09-05 17:01:52 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import AboutUs from './pages/AboutUs';
-import Blog from './pages/Blog';
-import Buy from './pages/Buy';
-import Contacts from './pages/Contacts';
-import Home from './pages/Home';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import './index.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import Blog from "./pages/Blog";
+import Buy from "./pages/Buy";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
-  return (
-    <Router>
-      <span>Perevoda poka ne budet. Etot font-style ne podderjivaet ruskiy.</span>
-      <Navigation/>
-      <Routes>
-      <Route path='/' exact element={<Home/>} />
-      <Route path='/AboutUs' exact element={<AboutUs/>} />
-      <Route path='/contacts' exact element={<Contacts/>} />
-      <Route path='/Buy' exact element={<Buy/>} />
-      <Route path='/Blog' exact element={<Blog/>} />
-      <Route path='/AboutUs' exact element={<AboutUs/>} />
-      </Routes>
-      <Footer/>
-    </Router>
-  );
+    return (
+        <Router>
+            <span>
+                Perevoda poka ne budet. Etot font-style ne podderjivaet ruskiy.
+                TEST
+            </span>
+            <Navigation />
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/AboutUs" exact element={<AboutUs />} />
+                <Route path="/contacts" exact element={<Contacts />} />
+                <Route path="/Buy" exact element={<Buy />} />
+                <Route path="/Blog" exact element={<Blog />} />
+                <Route path="/AboutUs" exact element={<AboutUs />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
